c7e6cdc063bf6c7545bca21772a8c32a29b6c587
Resal Ramdahadi <resalramdahadi92@gmail.com>
2018-06-27 09:06:57 +0700

    starter_burger

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+
 import classes from "./BurgerIngredient.css";
 
 class BurgerIngredient extends Component {
@@ -38,7 +39,7 @@ class BurgerIngredient extends Component {
   }
 }
 
-BurgerIngredient.PropTypes = {
+BurgerIngredient.propTypes = {
   type: PropTypes.string.isRequired
 };
 
