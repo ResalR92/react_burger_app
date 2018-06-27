32ad916f6d48191177d13dc91ce0564ada207f91
Resal Ramdahadi <resalramdahadi92@gmail.com>
2018-06-27 08:30:08 +0700

    creating_first_layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Aux from "../../hoc/Aux";
+
+const Layout = props => {
+  return (
+    <Aux>
+      Toolbar, SideDrawer, Backdrop
+      <main>{props.children}</main>
+    </Aux>
+  );
+};
+
+export default Layout;
