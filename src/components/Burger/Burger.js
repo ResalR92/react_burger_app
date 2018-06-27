efaf772626f8524247fb4834fce862b3e5344405
Resal Ramdahadi <resalramdahadi92@gmail.com>
2018-06-27 09:59:47 +0700

    burger_dynamic_ingredients_sum

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,11 +6,19 @@ import classes from "./Burger.css";
 
 const Burger = props => {
   // convert object to array.map()
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el); // return empty array if there is nothing (ingredient)
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
