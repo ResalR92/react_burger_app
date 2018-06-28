f59ea6907f3dd5c59425253863bf6bfa601fa29f
Resal Ramdahadi <resalramdahadi92@gmail.com>
2018-06-28 12:49:36 +0700

    layout_toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+
 import classes from "./Layout.css";
 
 const Layout = props => {
   return (
     <Aux>
-      Toolbar, SideDrawer, Backdrop
+      <Toolbar />
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
