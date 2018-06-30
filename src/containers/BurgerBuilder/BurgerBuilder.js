bbeee73bcff4caeee899996de920063264e914d3
Resal Ramdahadi <resalramdahadi92@gmail.com>
2018-06-30 13:45:51 +0700

    navigating_to_checkout_from_continue_button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,31 +95,33 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Resal Ramdahadi",
-        address: {
-          street: "Hanura street",
-          zipCode: "35454",
-          country: "Republik Hanura"
-        },
-        email: "resal@hanura.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        console.log(response);
-        this.setState({ loading: false, purchashing: false });
-      })
-      .catch(error => {
-        console.log(error);
-        this.setState({ loading: false, purchashing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Resal Ramdahadi",
+    //     address: {
+    //       street: "Hanura street",
+    //       zipCode: "35454",
+    //       country: "Republik Hanura"
+    //     },
+    //     email: "resal@hanura.com"
+    //   },
+    //   deliveryMethod: "fastest"
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then(response => {
+    //     console.log(response);
+    //     this.setState({ loading: false, purchashing: false });
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //     this.setState({ loading: false, purchashing: false });
+    //   });
+
+    this.props.history.push("/checkout");
   };
 
   render() {
