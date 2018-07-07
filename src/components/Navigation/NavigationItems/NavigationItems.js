ca350399762071230512e0464ddb34cd6128e007
Resal Ramdahadi <resalramdahadi92@gmail.com>
2018-07-07 14:46:20 +0700

    create_auth_container_and_auth_actions_creator_basic

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -11,6 +11,7 @@ const NavigationItems = props => {
         Burger Builder
       </NavigationItem>
       <NavigationItem link="/orders">Orders</NavigationItem>
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
     </ul>
   );
 };
