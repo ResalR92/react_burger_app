2858b79275d9dc0ea609ada840446ac3c0948f3a
Resal Ramdahadi <resalramdahadi92@gmail.com>
2018-07-08 09:40:00 +0700

    redirect_unauth_order_to_auth_then_to_checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,6 +98,7 @@ class BurgerBuilder extends Component {
     if (this.props.isAuthenticated) {
       this.setState({ purchashing: true });
     } else {
+      this.props.onSetAuthRedirectPath("/checkout");
       this.props.history.push("/auth");
     }
   };
@@ -207,7 +208,8 @@ const mapDispatchToProps = dispatch => {
     //   ingredientName: ingName
     // })
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: path => dispatch(actions.setAuthRedirectPath(path))
   };
 };
 
